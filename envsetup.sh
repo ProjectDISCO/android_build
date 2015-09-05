18b74191a1061c8e0c9d14b4f0d07604e3220c74
Yashar Ahmedizade <yashade2001@gmail.com>
2015-09-05 14:12:44 +0300

    envsetup: Point the default combo to hammerhead
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -478,7 +478,7 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo -n "Which would you like? [aosp_arm-eng] "
+        echo -n "Which would you like? [disco_hammerhead-userdebug] "
         read answer
     fi
 
@@ -486,7 +486,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_arm-eng
+        selection=disco_hammerhead-userdebug
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         if [ $answer -le ${#LUNCH_MENU_CHOICES[@]} ]
