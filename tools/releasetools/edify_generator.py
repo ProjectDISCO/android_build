6b6f442b8fc3cc651036d96635a8eb008f3a42ba
Yashar Ahmedizade <yashade2001@gmail.com>
2015-10-30 15:54:51 +0200

    Revert "Remove nulls from the edify generator."
    This reverts commit 5e1baeab0ee3891a5f2e76f72fc4fcecfbcfdd90.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -126,7 +126,7 @@ class EdifyGenerator(object):
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
     cmd = ("assert(" +
-           " || ".join(['getprop("ro.bootloader") == "%s"' % (b,)
+           " ||\0".join(['getprop("ro.bootloader") == "%s"' % (b,)
                          for b in bootloaders]) +
            ");")
     self.script.append(self.WordWrap(cmd))
